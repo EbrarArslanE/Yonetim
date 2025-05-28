14b643761a78ff39563173e1187f0150e9847f02
Ebrar Arslan <ebrariphone0@icloud.com>
2025-05-28 13:37:57 +0300

    v2

--- musteriTalebi/public/pages/projeDetay/projeDetay.js
+++ musteriTalebi/public/pages/projeDetay/projeDetay.js
@@ -14,19 +14,19 @@ window.onload = () => {
           return;
         }
 
-        document.getElementById('e_proje_adi').textContent = proje.e_proje_adi;
-        document.getElementById('e_proje_yetkilisi').textContent = proje.e_proje_yetkilisi;
-        document.getElementById('e_firma_adi').textContent = proje.e_firma_adi;
-        document.getElementById('e_durum').textContent = proje.e_durum;
-        document.getElementById('e_proje_alim_tarihi').textContent = proje.e_proje_alim_tarihi;
-        document.getElementById('e_proje_teslim_tarihi').textContent = proje.e_proje_teslim_tarihi;
-        document.getElementById('e_mail_bilgisi').textContent = proje.e_mail_bilgisi;
-        document.getElementById('e_telefon_bilgisi').textContent = proje.e_telefon_bilgisi;
-        document.getElementById('e_ekip_uyeleri').textContent = proje.e_ekip_uyeleri;
-        document.getElementById('e_proje_tipi').textContent = proje.e_proje_tipi;
-        document.getElementById('e_git_repo_linki').href = proje.e_git_repo_linki;
-        document.getElementById('e_oncelik').textContent = proje.e_oncelik;
-        document.getElementById('e_fiyat').textContent = proje.e_fiyat;
+        document.getElementById('e_proje_adi').value = proje.e_proje_adi;
+        document.getElementById('e_proje_yetkilisi').value = proje.e_proje_yetkilisi;
+        document.getElementById('e_firma_adi').value = proje.e_firma_adi;
+        document.getElementById('e_durum').value = proje.e_durum;
+        document.getElementById('e_proje_alim_tarihi').value = proje.e_proje_alim_tarihi;
+        document.getElementById('e_proje_teslim_tarihi').value = proje.e_proje_teslim_tarihi;
+        document.getElementById('e_mail_bilgisi').value = proje.e_mail_bilgisi;
+        document.getElementById('e_telefon_bilgisi').value = proje.e_telefon_bilgisi;
+        document.getElementById('e_ekip_uyeleri').value = proje.e_ekip_uyeleri;
+        document.getElementById('e_proje_tipi').value = proje.e_proje_tipi;
+        document.getElementById('e_git_repo_linki').textContent = proje.e_git_repo_linki;
+        document.getElementById('e_oncelik').value = proje.e_oncelik;
+        document.getElementById('e_fiyat').value = proje.e_fiyat;
       })
       .catch(err => console.error("Detay getirme hatası:", err));
   }
