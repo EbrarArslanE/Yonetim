646218b5fe1b8707089a7a398f4e2c7575096ce7
Ebrar Arslan <ebrariphone0@icloud.com>
2025-05-29 13:20:44 +0300

    v1

---
+++ musteriTalebi/public/pages/route.js
@@ -0,0 +1,12 @@
+function kullaniciTanimlari(params) {
+    window.location.href = "/pages/kullaniciListesi/kullaniciListesi.html";
+}
+function gorevTanimlari(params) {
+    window.location.href = "/pages/gorevListesi/gorevListesi.html";
+}
+function talepTanimlari(params) {
+    window.location.href = "/pages/kayitListesi/kayitListesi.html";
+}
+function projeTanimlari(params) {
+    window.location.href = "/pages/projeListesi/projeListesi.html";
+}
